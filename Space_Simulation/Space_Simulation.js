6e23632e12ff38f9125e0df1478d2e305628bb57
Jared Kimbel <1255376@ad.ccsd.net>
2017-02-14 14:10:31 -0800

    Added preload function
    preload imports all images in the assets folder
    draw loop now steps through all elements of images array and draws
    center point at 0,0

--- Space_Simulation/Space_Simulation.js
+++ Space_Simulation/Space_Simulation.js
@@ -1,5 +1,23 @@
 var stars = []
 var speed = 10;
+var images = []
+
+function preload() 
+{
+  imageMode(CENTER);
+  
+  images[10] = loadImage("assets/sun.png");
+  images[9] = loadImage("assets/mercury.png");
+  images[8] = loadImage("assets/venus.png"); 
+  images[7] = loadImage("assets/earth.png");
+  images[6] = loadImage("assets/moon.png");
+  images[5] = loadImage("assets/mars.png");
+  images[4] = loadImage("assets/jupiter.png");
+  images[3] = loadImage("assets/saturn.png");
+  images[2] = loadImage("assets/uranus.png");
+  images[1] = loadImage("assets/neptune.png");
+  images[0] = loadImage("assets/pluto.png");
+}
 
 function setup() 
 {
@@ -14,8 +32,14 @@ function setup()
 function draw() 
 {
   background(0);
-
+  
   translate(mouseX , mouseY);
+  
+  for (var i = 0; i < images.length; i++) 
+  {
+    image(images[i], 0, 0, images[0].width/4, images[0].height/4);
+  }
+  
   
   for (var i = 0; i < stars.length; i++) 
   {
