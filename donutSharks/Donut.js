050faebb6885c7d4da9536f6597e121b35c0341e
Jared <kimbeljared@gmail.com>
2018-12-13 11:53:36 -0800

    Bound Circles to box

--- donutSharks/Donut.js
+++ donutSharks/Donut.js
@@ -3,6 +3,7 @@ function Donut(x, y, r)
   this.r = r
   this.v = createVector(x, y);
   this.speed = 5;
+  this.dir = 1
 
   this.show = function()
   {
@@ -10,6 +11,7 @@ function Donut(x, y, r)
 
     noFill();
 
+    ellipseMode(CENTER);
     ellipse(this.v.x, this.v.y, this.r, this.r);
 
     pop();
@@ -17,7 +19,26 @@ function Donut(x, y, r)
 
   this.move = function()
   {
-    this.v.x = this.v.x + this.speed;
-    
+    this.v.x = this.v.x + (this.speed * this.dir);
+
+    if (this.v.x >= (width-this.r/2))
+    {
+        this.dir = -1;
+    }
+    else if (this.v.x <=  (0+this.r/2))
+    {
+        this.dir = 1;
+    }
+
+    this.v.y = this.v.y + (this.speed * this.dir);
+
+    if (this.v.y >= (height-this.r/2))
+    {
+        this.dir = -1;
+    }
+    else if (this.v.y <= (0+this.r/2))
+    {
+        this.dir = 1;
+    }
   }
 }
--- donutSharks/sketch.js
+++ donutSharks/sketch.js
@@ -1,10 +1,10 @@
 var donuts = [];
-
+var numDonuts = 1;
 function setup()
 {
   createCanvas(400, 400);
 
-  for(var i = 0; i < 10; i++)
+  for(var i = 0; i < numDonuts; i++)
   {
     donuts[i] = new Donut(25+(i*30),200, 25)
   }
@@ -14,8 +14,10 @@ function draw()
 {
   background(51);
 
-  for(var i = 0; i < 10; i++)
+  for(var i = 0; i < numDonuts; i++)
   {
     donuts[i].show();
+    donuts[i].move();
+    console.log(donuts[i].v.heading());
   }
 }
