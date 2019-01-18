9fabee7005d6b12f0ecf3d8a0974c1e3c7055f1e
Jared <kimbeljared@gmail.com>
2019-01-18 15:12:14 -0800

    More Esp stuff
    Recursive points are made, need to make multiple children possible tho.

--- recursiveSocExample/point.js
+++ recursiveSocExample/point.js
@@ -4,6 +4,16 @@ class point
   constructor(px, py)
   {
       this.x = px + xOffset;
-      this.y = (py * ladder())
+      this.y = py + (ladder() * random(childYOffsets));
+  }
+
+  show()
+  {
+      push();
+
+      noFill();
+      ellipse(this.x, this.y, 5);
+
+      pop();
   }
 }
--- recursiveSocExample/sketch.js
+++ recursiveSocExample/sketch.js
@@ -1,21 +1,47 @@
-var xOffset = 5;
+var xOffset = 10;
 var points = [];
-
+var childYOffsets = [5, 10, 15, 20];
+var numOfChild = [1, 2, 3];
+var coinFlip = [-1, 1];
 function setup()
 {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
+  makePoints(xOffset, height/2);
 }
 
 function draw()
 {
   background(51);
 
-  var test = new point(50,50);
-  points.push(test)
+  for (i = 0; i <  points.length;  i++)
+  {
+    points[i].show();
+  }
 }
 
 function ladder()
 {
-  var val = int(random (0,2));
+  var val = random(coinFlip);
   return val;
+}
+
+function makePoints(px,py, mc)
+{
+  if (points.length < 50 || mc ==  0)
+  {
+    var aPoint = new point(px,py);
+    points.push(aPoint);
+
+    var child = random(numOfChild);
+    var i = 1;
+    do {
+      makePoints(aPoint.x, aPoint.y);
+      i++
+    } while (i < child);
+  }
+  if (points.length < 50 || mc > 0)
+  {
+    var aPoint = new point(px,py);
+    points.push(aPoint);
+  }
 }
