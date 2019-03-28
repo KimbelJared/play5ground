77050611df2ffcea0af482672c3deb9300589a7d
Jared Kimbel <KimbelJared@gmail.com>
2019-03-28 14:32:52 -0700

    CANT GET THE COLORS TO WORK

--- recursiveSocExample/point.js
+++ recursiveSocExample/point.js
@@ -1,18 +1,26 @@
 class point
 {
 
-  constructor(px, py)
+  constructor(px, py, mc)
   {
+      this.px = px;
+      this.py = py;
       this.x = px + xOffset;
       this.y = py + (ladder() * random(childYOffsets));
+      this.gen = (this.x/xOffset)-2;
+      console.log("gen: " + this.gen)
+      console.log("x: " + this.x)
   }
 
-  show()
+  show(passedColor)
   {
       push();
 
-      noFill();
+      stroke(255);
+
       ellipse(this.x, this.y, 5);
+
+      line(this.px, this.py, this.x, this.y);
 
       pop();
   }
--- recursiveSocExample/sketch.js
+++ recursiveSocExample/sketch.js
@@ -1,11 +1,20 @@
-var xOffset = 10;
+var xOffset = 25;
 var points = [];
-var childYOffsets = [5, 10, 15, 20];
+var childYOffsets = [25, 50, 75, 100];
 var numOfChild = [1, 2, 3];
 var coinFlip = [-1, 1];
+var colors = [];
+
 function setup()
 {
-  createCanvas(400, 400);
+  createCanvas(1270, 800);
+
+
+  for (i = 0; i < 50; i++)
+  {
+    colors[i] = color(random(0,255),random(0,255),random(0,255));
+  }
+
   makePoints(xOffset, height/2);
 }
 
@@ -15,8 +24,22 @@ function draw()
 
   for (i = 0; i <  points.length;  i++)
   {
-    points[i].show();
+    points[i].show(colors[points[i].gen]);
   }
+
+  var z = 1;
+  for (i = xOffset; i <  width;  i+= xOffset)
+  {
+    push();
+    noStroke();
+    fill(colors[z-1]);
+    text(z, i, 50);
+    pop();
+
+    z++
+
+  }
+
 }
 
 function ladder()
@@ -29,19 +52,19 @@ function makePoints(px,py, mc)
 {
   if (points.length < 50 || mc ==  0)
   {
-    var aPoint = new point(px,py);
+    var aPoint = new point(px,py, mc);
     points.push(aPoint);
 
     var child = random(numOfChild);
     var i = 1;
     do {
-      makePoints(aPoint.x, aPoint.y);
+      makePoints(aPoint.x, aPoint.y, child);
       i++
     } while (i < child);
   }
   if (points.length < 50 || mc > 0)
   {
-    var aPoint = new point(px,py);
+    var aPoint = new point(px,py, mc);
     points.push(aPoint);
   }
 }
