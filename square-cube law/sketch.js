f358159f4baa2f0a48ec3d5f25af9783aeaea04f
Jared Kimbel <KimbelJared@gmail.com>
2019-04-25 14:46:12 -0700

    basic definitions of variables

--- square-cube law/sketch.js
+++ square-cube law/sketch.js
@@ -1,10 +1,32 @@
+var slider;
+
+var length, area_1, area_2, volume_1, volume_2;
+
 function setup()
 {
-  var canvas = createCanvas(800, 800);
-  canvas.parent('sketch-wrapper');
+  var canvas = createCanvas(800, 800, WEBGL);
+
+  slider = createSlider(50, 300, 100);
+  slider.position(10, 10);
 }
 
 function draw()
 {
   background(51);
+
+  length = slider.value();
+
+  area_1 = pow(length, 2);
+  volume_1 = pow(length, 3);
+
+  push();
+  rectMode(CENTER);
+  //square(width/2, height/2, length);
+  pop();
+
+  push();
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(length);
+  pop();
 }
