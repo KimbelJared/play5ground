3d11f6b784fb6bbca5fed8e3e76ae754a30537dd
Jared Kimbel <KimbelJared@gmail.com>
2019-06-21 14:27:13 -0700

    Gradient Lines Update
    more work on the gradient lines math
    as well as creation of the p5.utils library

--- gradientLines/finished/sketch.js
+++ gradientLines/finished/sketch.js
@@ -1,17 +1,30 @@
 let DOT_SIZE = 20;
 
-var dotA, dotB;
+var dotA, dotB, dotC;
+var lineD, lineE;
 
 function setup()
 {
+  startUrEngines(true);
+
   var canvas = createCanvas(800, 800);
-  dotA = new Point(200, 400);
-  dotB = new Point(600, 400);
+
+  dotA = new point(200, 400, color('#a1d9ff'));
+  dotB = new point(600, 200, color('#ca82f8'));
+  dotC = new point(600, 600, color('#f2bbbb'));
+
+  lineD = new fancyLine(dotA, dotB);
+  //lineE = new fancyLine(dotA, dotC);
 }
 
 function draw()
 {
   background(51);
+
   dotA.show();
   dotB.show();
+  dotC.show();
+
+  lineD.show();
+  //lineE.show();
 }
---
+++ p5.utils.js
@@ -0,0 +1,229 @@
+/*
+
+Jared Kimbel | Jaredkimbel.com
+Version 0.1 | June 21th, 2019
+
+*/
+console.log("p5.utils -- loaded");
+var DEBUG;
+
+//Setup library
+p5.prototype.startUrEngines = function(d)
+{
+  DEBUG = d;
+  if(DEBUG)
+  {
+    console.log("p5.utils -- debug enabled");
+  }
+}
+
+//Objects
+
+/*
+
+Point Object
+
+*/
+p5.prototype.point = class
+{
+  //Create all variables that the points will need
+  constructor(x, y, color)
+  {
+      this.x = x;
+      this.y = y;
+      this.color = color;
+  }
+  //Make sure point is in bounds
+  test()
+  {
+    if (this.y > height)
+    {
+      this.y = height;
+      console.log("y not in bounds, set to max");
+    } else if (this.y < 0)
+    {
+      this. y = 0;
+      console.log("y not in bounds, set to min");
+    } else if (this.x < 0)
+    {
+      this. x = 0;
+      console.log("x not in bounds, set to min");
+    } else if (this.x > width)
+    {
+      this. x = width;
+      console.log("x not in bounds, set to max");
+    }
+  }
+
+  //Diplay point on screen
+  show()
+  {
+      push();
+
+      this.test();
+
+      colorMode(RGB, 255)
+      fill(this.color);
+      noStroke();
+
+      ellipse(this.x, this.y, DOT_SIZE);
+
+      pop();
+  }
+}
+
+/*
+
+Line Object
+
+*/
+p5.prototype.fancyLine = class
+{
+  //Create all variables that the line will need
+  constructor(point1, point2)
+  {
+      this.x1 = point1.x;
+      this.y1 = point1.y;
+
+      this.x2 = point2.x;
+      this.y2 = point2.y;
+
+      this.color1 = point1.color;
+      this.color2 = point2.color;
+
+      this.slope = findSlope(point1, point2);
+      if(DEBUG) {console.log("Slope: "+this.slope);}
+
+      this.gradientLines = [];
+
+      if(this.color1 == this.color2)
+      {
+        if(DEBUG) {console.log("Colors are the same, no need for gradient");}
+
+      } else
+      {
+        if(DEBUG) {console.log("Colors are different, preparing gradient");}
+        this.gradientLines = createGradient(point1, point2, this.slope);
+      }
+  }
+
+  show()
+  {
+    line(this.x1, this.y1, this.x2, this.y2);
+
+    for(let i = 0; i < this.gradientLines.length; i++)
+    {
+      this.gradientLines[i].show();
+    }
+  }
+}
+
+/*
+
+Line Segment Object (for Gradient)
+
+*/
+p5.prototype.fancyLineSegment = class
+{
+  //Create all variables that the line will need
+  constructor(point1, point2, color)
+  {
+    this.x1 = point1.x;
+    this.y1 = point1.y;
+
+    this.x2 = point2.x;
+    this.y2 = point2.y;
+
+    this.color1 = point1.color;
+  }
+
+  show()
+  {
+    push();
+    stroke(this.color)
+    line(this.x1,this.y1,this.x2,this.y2);
+    pop();
+  }
+}
+
+//Functions
+
+/*
+
+Finding Slope Function
+
+*/
+p5.prototype.findSlope = function(point1, point2)
+{
+  //get needed variables
+  let x1 = point1.x;
+  let y1 = point1.y;
+
+  let x2 = point2.x;
+  let y2 = point2.y;
+
+  return((y2-y1)/(x2-x1));
+}
+
+/*
+
+Create Gradient Function
+
+*/
+p5.prototype.createGradient = function(startPoint, endPoint, slope)
+{
+  var lineArray = [];
+  var pointArray = [];
+
+  if(slope < 0)
+  {
+    if(DEBUG) {console.log("Slope < 0");}
+    pointArray[0] = createGradientPoint(startPoint, endPoint);
+    for(i=1; i<20; i++)
+    {
+      pointArray[i] = createGradientPoint(pointArray[i-1], endPoint);
+    }
+    //lineArray = createGradientLineSegments(pointsArray);
+  } else if (slope > 0)
+  {
+    if(DEBUG) {console.log("Slope > 0");}
+  } else
+  {
+    if(DEBUG) {console.log("Slope == 0");}
+  }
+  return pointArray;
+}
+
+/*
+
+Create Gradient Point Function
+
+*/
+p5.prototype.createGradientPoint = function(startPoint, endPoint)
+{
+  let sx = startPoint.x;
+  let sy = startPoint.y;
+
+  let ex = endPoint.x;
+  let ey = endPoint.y;
+
+  let delta = (ex-ey)/1000;
+
+  let midX = lerp(sx, ex, delta);
+  let midY = lerp(sy, ey, delta);
+  var tempPoint = new point(midX, midY, 255);
+
+  return tempPoint;
+}
+
+/*
+
+Create Gradient Line Segments Function
+
+*/
+p5.prototype.createGradientLineSegments = function(pointsArray)
+{
+  var arrayToReturn = [];
+
+  return(arrayToReturn);
+}
